0b3355624b5afc9f4467391d7f77889dfc5bce13
Sebastian <ssarmiento@cliandina.com>
2025-02-26 22:16:21 -0500

    add

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://Sebastian-IngSoftware.github.io',
+  base: 'portfolio',
 
-    site: 'https://Sebastian-IngSoftware.github.io',
-    base: 'portfolio',
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
